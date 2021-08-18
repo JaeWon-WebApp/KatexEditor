8c83963b9e599a991cd4b4850dc0775a90da4717
JaeWon <iixxz25@gmail.com>
2021-08-18 10:16:12 +0900

    0818

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import renderMathInElement from 'katex/dist/contrib/auto-render';
+import 'katex/dist/katex.min.css';
+import TexEditor from './TexEditor';
 
 function App() {
+  const [boardWrite, setBoardWrite] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [range, setRange] = useState(null);
+  const boardRef = useRef();
+  const katexPreviewRef = useRef();
+
+  useEffect(() => {
+    renderMathInElement(katexPreviewRef.current);
+  }, [boardWrite]);
+
+  const texEditorOpen = () => {
+    let selection = window.getSelection().containsNode(boardRef.current, true);
+    if(!selection)
+      boardRef.current.focus();
+    setRange(window.getSelection().getRangeAt(0));
+    setModalOpen(true);
+  };
+
+  const texEditorClose = () => {
+    setModalOpen(false);
+  }
+
+  const boardChange = (e) => {
+    console.log(boardWrite);
+    setBoardWrite(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="boardWrapper">
+        <div id="bar">
+          <button onClick={texEditorOpen}>수식</button>
+        </div>
+        <textarea id="board" ref={boardRef} value={boardWrite} onChange={boardChange}>
+
+        </textarea>
+        <div id="boardPreview" ref={katexPreviewRef}>
+          {boardWrite}
+        </div>
+      </div>
+      {modalOpen &&
+        <TexEditor
+          texEditorClose={texEditorClose}
+          range={range}
+        />
+      }
+    </>
   );
 }
 
---
+++ src/TexEditor.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './TexEditor.css';
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
+
+function TexEditor(props) {
+  const texs = ["x_x", "x^y", "\\frac{2}{3}", "\\boxed{\\pi=\\frac c d}", "\\infty", "\\to", "\\gets", "\\sqrt{2}"];
+  const [texStr, setTexStr] = useState("");
+  const previewRef = useRef();
+
+  useEffect(() => {
+    const renderTex = () => {
+      katex.render(texStr, previewRef.current, {
+        throwOnError: false,
+      });
+    };
+
+    renderTex();
+  }, [texStr]);
+
+  const texChange = (e) => {
+    console.log(e.target.value);
+    setTexStr(e.target.value);
+  }
+
+  const insertTex = () => {
+    let node = document.createTextNode(`$$${texStr}$$`);
+    props.range.deleteContents();
+    props.range.insertNode(node);
+    modalClose();
+  }
+
+  const modalClose = () => {
+    setTexStr("");
+    props.texEditorClose();
+  }
+
+  return (
+    <>
+      <div id="texWrapper">
+        <div id="texEditor">
+          <div id="texBar">
+            <div id="texBtnArea">
+              {texs.map((tex, index) => (
+                <button key={index} className="texBtn" onClick={() => setTexStr(texStr + tex)}>
+                  <img src={`images/${index + 1}.png`} alt="" />
+                </button>
+              ))}
+            </div>
+          </div>
+          <div id="writeArea">
+            <div id="writePreview" ref={previewRef}>
+            </div>
+            <textarea id="texWrite" value={texStr} onChange={texChange} />
+          </div>
+          <div id="insertCloseBtn">
+            <button onClick={insertTex}>입력</button>
+            <button onClick={modalClose}>닫기</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TexEditor
